9e7226e4b6294f43474d301fc785ec85ef2b036b
SebasProgram777 <juanpro15m@gmail.com>
2025-04-11 21:01:44 -0500

    listas agregadas

---
+++ funciones.py
@@ -0,0 +1,35 @@
+def resta(a,b):
+
+    return a-b
+
+def mensaje(x):
+    print("Alerta: ", x)
+
+def multiplicacion(a:int,b:int) -> int:
+
+    return a*b
+
+def saludo(a:str) -> str:
+
+    x = "Hola "+ a
+
+    return x
+
+def despedida(a:str) -> None:
+
+    x = "Chao "+ a
+
+    print(x)
+#####
+
+c = 5
+d : int = 2
+
+print(resta(c,d))
+
+print(multiplicacion(c,d))
+
+print(saludo("Carlos"))
+
+despedida("Carlos")
+mensaje("Mamerto")
